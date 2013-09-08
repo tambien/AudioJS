df90f7f61b598a8f10754e071d98803ad3fa7af8
Yotam Mann <yotammann@gmail.com>
2013-09-08 18:32:20 -0400

    updated OSC

--- components/OSC.js
+++ components/OSC.js
@@ -1,18 +1,53 @@
+"use strict";
+
 /*=============================================================================
 	OSC-Style message/event mediatator pattern
 =============================================================================*/
 
-AUDIO.OSC
+AUDIO.OSC = {};
 
-	/*=========================================================================
-		SCHEDULER KEEPS A CLOCK GOING
-	=========================================================================*/
+/*=========================================================================
+		SCHEDULER
+=========================================================================*/
 
-	SCHEDULER = function(){
-		addMessage : function ( msg ){
-			
-		}
-	};
+AUDIO.OSC.SCHEDULER = function(){
+	addMessage : function ( msg ){
 
+	}
+}
+
+	
+
+/*=========================================================================
+	MESSAGE
+=========================================================================*/
+/* 
+	@param {string} address
+	@param {Object} data
+	@constructor
+*/
+AUDIO.OSC.MESSAGE  = function(address, data){
+	this.address = address;
+	this.data = data;
 };
 
+
+/*=========================================================================
+	PACKET
+=========================================================================*/
+/* 
+	@param {number} timestamp (in milliseconds)
+	@param {Array.<Object>=} messages
+	@constructor
+*/
+AUDIO.OSC.PACKET  = function(timestamp, messages){
+	this.timestamp = parseInt(timestamp);
+	this.messages = messages||[];
+};
+
+/* 
+	@param {Object | Array.<Object>=} messages
+*/
+AUDIO.OSC.PACKET.prototype.addMessage = function(messages){
+	Object.prototype.toString.call(messages) == "[object Array]";
+}
