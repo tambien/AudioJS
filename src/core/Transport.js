10cbceb7714e13ebb1dbd879f0e29499910736ae
Yotam Mann <yotammann@gmail.com>
2013-09-10 14:09:20 -0400

    start/stop/pause on transport

--- src/core/Transport.js
+++ src/core/Transport.js
@@ -4,21 +4,62 @@
 	keeps track of the duration of the 
 =============================================================================*/
 
+"use strict";
+
 AUDIO.TRANS = {
 	/* @private */
-	currentTime : 0,
+	startTime : 0,
+	/* @private */
+	pauseDuration : 0,
+	/* @private */
+	pausedTime : 0,
 	/* @private */
 	state : 'READY',
 	/* start / restart the transport */
 	start : function(){
-		this.state = started
+		if (this.state === "READY" || this.state === "STOPPED"){
+			this.startTime = AUDIO.context.currentTime;
+		} else if (this.state ==="PAUSED"){
+			this.pauseDuration += AUDIO.context.currentTime - this.pausedTime;
+		}
+		this.state = "STARTED";
 	},
 	/* stop the metro */
 	stop : function(){
-
+		this.state = "STOPPED";
+		this.startTime = 0;
+		this.pauseDuration = 0;
+		this.pausedTime = 0;
 	},
 	/* pause it */
 	pause : function(){
+		if (this.state === "STARTED"){
+			this.state = "PAUSED";
+			this.pausedTime = AUDIO.context.currentTime;
+		}
+	},
+	/*
+		@return {number} elapsed time in seconds
+	*/
+	getTime : function(){
+		if (this.state === "STARTED"){
+			return AUDIO.context.currentTime - this.startTime - this.pauseDuration;
+		} else if (this.state === "PAUSED"){
+			return this.pausedTime - this.startTime - this.pauseDuration ;
+		} else {
+			return 0;
+		}
+	},
+	/*
+		@return {number} the measure number
+	*/
+	getMeasure : function(){
+		var elapsedTime = this.getTime();
+	},
+	/*
+		@return {number} the beat number
+	*/
+	getBeat : function(){
 
 	}
 }
